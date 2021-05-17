1664f4083a13fc3135d11766734c84e0b9aaa85a
Ryder <rdeback@gmail.com>
2021-05-17 09:49:21 -0700

    Base startup for the cookie practice.

---
+++ TS/main.ts
@@ -0,0 +1,6 @@
+window.onload = function()
+{
+    let readCookie = document.getElementById("readCookie");
+    let deleteCookie = document.getElementById("deleteCookie");
+    let createCookie = document.getElementById("createCookie");
+}
